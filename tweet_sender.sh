516a0398563e135f78b7a18973928bc5c69d3418
aarushi <aarushi12002@iiitd.ac.in>
2022-06-12 12:58:27 -0700

    "extracts naval's tweet and sends via whatsapp"

--- tweet_sender.sh
+++ tweet_sender.sh
@@ -1,8 +1,11 @@
+#!/bin/zsh
 tweet1=$(curl -X GET -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAARQdgEAAAAAEbecLt5PiXS5bfWHxN1uRligjk8%3DtLBTnOzYJzlB31gTuFPB1rUyKJ9fj2K9IRxTvSdJI417JnjgvR"  \
 "https://api.twitter.com/2/tweets/search/recent?query=from:NavalismHQ&tweet.fields=created_at" >twitter_file.json | jq '.meta' | jq '.newest_id' | tr -d '"')
 tweet1=`echo "$tweet1" | tr -d '"'`
+echo $tweet1
 var="$(curl -X GET -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAARQdgEAAAAAEbecLt5PiXS5bfWHxN1uRligjk8%3DtLBTnOzYJzlB31gTuFPB1rUyKJ9fj2K9IRxTvSdJI417JnjgvR" "https://api.twitter.com/2/tweets?ids=$tweet1" | jq ".data[].text " ; echo)"
 JSON_STR=$(jq -n --arg v "$var" '{ "messaging_product": "whatsapp", "to": "12065814687", "type": "text", "text": { "body": $v } }' )
+echo $JSON_STR
 curl -i -X POST \
   https://graph.facebook.com/v13.0/101077889310243/messages \
   -H 'Authorization: Bearer EAAFZCS86rXNoBAKLEdY0dpc4RSZAw0iZCN9CCl2k2laDh18gAUdxeWHQ5IhWmZBZBTJ5EcBGGu8aj591U32PRZBqg2e5Xvbh0RDVeZCMbO4UsWvo3M4N2SxJ15TmA2Jbs6a9ZBZBr923gd6MSAbKHt9bWH53vDRL3q8C8OP8WyDnoOAZDZD' \
